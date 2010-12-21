Cloudxtreme/gfarm

commit 89f197e3d2b5dabfd415c7f66ba394feca184a69

fix invalid memory read in inode.c
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4969 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -3864,7 +3864,8 @@ inode_has_desired_number(struct inode *inode, int *desired_numberp)
 {
 	void *value;
 	size_t size;
-	char *s;
+	unsigned char *s;
+	int i, n;
 
 	if (inode_xattr_get_cache(inode, 0, "gfarm.ncopy", &value, &size) !=
 	    GFARM_ERR_NO_ERROR)
@@ -3872,10 +3873,14 @@ inode_has_desired_number(struct inode *inode, int *desired_numberp)
 
 	if (value == NULL)
 		return (0);
-	for (s = value; *s == ' ' || *s == '\t'; s++)
+	s = value;
+	for (i = 0; i < size && isspace(s[i]); i++)
 		;
-	if (isdigit(*(unsigned char *)s)) {
-		*desired_numberp = atoi(s);
+	if (i < size && isdigit(s[i])) {
+		n = 0;
+		for (i = 0; i < size && isdigit(s[i]); i++)
+			n = n * 10 + (s[i] - '0');
+		*desired_numberp = n;
 		free(value);
 		return (1);
 	}

commit 0bb7bbbfff37be0136d371d616cb15f4df2b1734

fix a bug introduced in the previous r4969
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@4970 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -3878,7 +3878,7 @@ inode_has_desired_number(struct inode *inode, int *desired_numberp)
 		;
 	if (i < size && isdigit(s[i])) {
 		n = 0;
-		for (i = 0; i < size && isdigit(s[i]); i++)
+		for (; i < size && isdigit(s[i]); i++)
 			n = n * 10 + (s[i] - '0');
 		*desired_numberp = n;
 		free(value);