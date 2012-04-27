Cloudxtreme/gfarm

commit 90cac97e65f10b44fbf537e3ec70f8e91ab16882

implement #142 - generic send queue for backchannel for gfmd. this fixes the following problems:  * #76 - lock contention about back channel processing  * #79 - gfmd gives up too soon about GFS_PROTO_STATUS  * #184 - possible race between generation update and replication
this change partially addresses #42 (deadlock in gfmd) as well,
but a problem still exists in source:gfarm_v2/trunk/server/gfmd/file_replication.c@6235#L390


git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6235 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/dead_file_copy.h
@@ -1,8 +1,10 @@
 struct dead_file_copy;
+struct dead_file_copy_list;
 struct host;
 
 struct dead_file_copy *dead_file_copy_new(
-	gfarm_ino_t, gfarm_uint64_t, struct host *);
+	gfarm_ino_t, gfarm_uint64_t, struct host *,
+	struct dead_file_copy_list **);
 gfarm_ino_t dead_file_copy_get_ino(struct dead_file_copy *);
 gfarm_uint64_t dead_file_copy_get_gen(struct dead_file_copy *);
 struct host *dead_file_copy_get_host(struct dead_file_copy *);
@@ -10,23 +12,26 @@ struct host *dead_file_copy_get_host(struct dead_file_copy *);
 int dead_file_copy_is_removable_default(struct dead_file_copy *);
 extern int (*dead_file_copy_is_removable)(struct dead_file_copy *);
 
-void removal_pendingq_enqueue(struct dead_file_copy *);
-struct dead_file_copy *removal_pendingq_dequeue(void);
-void removal_finishedq_enqueue(struct dead_file_copy *, gfarm_int32_t);
-void host_busyq_enqueue(struct dead_file_copy *);
+void dead_file_copy_list_add(struct dead_file_copy_list **,
+	struct dead_file_copy *);
+int dead_file_copy_list_free_check(struct dead_file_copy_list *);
 
-void dead_file_copy_host_becomes_down(struct host *);
+void removal_pendingq_enqueue(struct dead_file_copy *);
+
 void dead_file_copy_host_becomes_up(struct host *);
 void dead_file_copy_host_removed(struct host *);
-void dead_file_copy_replica_status_changed(gfarm_ino_t, struct host *);
-void dead_file_copy_inode_status_changed(gfarm_ino_t);
+struct dead_file_copy_list;
+void dead_file_copy_inode_status_changed(struct dead_file_copy_list *);
 
 void dead_file_copy_mark_kept(struct dead_file_copy *);
-void dead_file_copy_mark_deferred(struct dead_file_copy *);
 
-int dead_file_copy_count_by_inode(gfarm_ino_t, gfarm_uint64_t, int);
-gfarm_error_t dead_file_copy_info_by_inode(gfarm_ino_t, gfarm_uint64_t, int,
-	int *, char **, gfarm_int64_t *, gfarm_int32_t *);
+int dead_file_copy_count_by_inode(struct dead_file_copy_list *,
+	gfarm_uint64_t, int);
+gfarm_error_t dead_file_copy_info_by_inode(struct dead_file_copy_list *,
+	gfarm_uint64_t, int, int *, char **, gfarm_int64_t *, gfarm_int32_t *);
 
 void dead_file_copy_init_load(void);
 void dead_file_copy_init(int);
+
+struct netsendq_type;
+extern struct netsendq_type gfs_proto_fhremove_queue;

commit 068d578da18c4e9fc294b10ee7fcc73444d15e82

fix gfmd segmentation fault like the followings:
{{{
#0  netsendq_was_sent_to_host (qhost=0x0) at netsendq.c:311
#1  0x0000000000425993 in gfs_client_fhremove_request (closure=0x22f0ff0)
    at dead_file_copy.c:420
#2  0x00000000004172e3 in thrpool_worker (arg=0x2201bf0) at thrpool.c:142
#3  0x00007f614ede0e9a in start_thread (arg=0x7f61017fa700)
    at pthread_create.c:308
#4  0x00007f614eb0e4bd in clone ()
    at ../sysdeps/unix/sysv/linux/x86_64/clone.S:112
#5  0x0000000000000000 in ?? ()
}}}

{{{
#0  abstract_host_to_host (h=0x0) at abstract_host.c:147
147             return (h->ops->abstract_host_to_host(h));
(gdb) bt
#0  abstract_host_to_host (h=0x0) at abstract_host.c:147
#1  0x0000000000429e20 in dead_file_copy_host_removed (host=0x7f9260018850)
    at dead_file_copy.c:200
#2  0x000000000041fbce in host_remove_internal (update_netsendq=1,
    hostname=0x7f926c0110c0 "tmphostname-gfm00-8332") at host.c:306
#3  host_remove (hostname=0x7f926c0110c0 "tmphostname-gfm00-8332")
    at host.c:317
#4  host_info_remove_default (
    hostname=0x7f926c0110c0 "tmphostname-gfm00-8332",
    diag=0x47fe50 "GFM_PROTO_HOST_INFO_REMOVE") at host.c:1717
#5  0x000000000042229b in gfm_server_host_info_remove (peer=0x7f929c1dd3b8,
    xid=0, sizep=0x0, from_client=1, skip=0) at host.c:1764
#6  0x0000000000416382 in protocol_switch (peer=0x7f929c1dd3b8, xid=0,
    sizep=0x0, from_client=1, skip=0, level=0, requestp=0x7f924d7f1e70,
    on_errorp=0x7f924d7f1e78, suspendedp=0x7f924d7f1e7c) at gfmd.c:179
#7  0x0000000000416bb2 in protocol_service (peer=0x7f929c1dd3b8, sizep=0x0,
    xid=0) at gfmd.c:680
#8  0x0000000000416e48 in protocol_main (arg=0x7f929c1dd3b8) at gfmd.c:795
#9  0x0000000000417f83 in thrpool_worker (arg=0xbe1330) at thrpool.c:142
#10 0x00007f92a165fe9a in start_thread (arg=0x7f924d7f2700)
    at pthread_create.c:308
#11 0x00007f92a138d4bd in clone ()
    at ../sysdeps/unix/sysv/linux/x86_64/clone.S:112
#12 0x0000000000000000 in ?? ()
}}}

these problems were introduced in r6235 (for #142 - generic send queue for backchannel for gfmd),

server/gfmd/dead_file_copy.c
@@ -34,7 +34,9 @@ struct dead_file_copy {
 	gfarm_ino_t inum;
 	gfarm_uint64_t igen;
 
-	int is_kept; /* on dfc_keptq?: protected by giant lock */
+	int dfc_flags;	/* protected by giant lock */
+#define DFC_FLAG_KEPT		1 /* on dfc_keptq? */
+#define DFC_FLAG_KEPT_HARD	2 /* removed when replication is completed */
 
 	GFARM_HCIRCLEQ_ENTRY(dead_file_copy) same_inode_copies;
 };
@@ -52,12 +54,22 @@ struct dfc_keptq {
 /* IMPORTANT NOTE: functions should not sleep while holding dfc_keptq.mutex */
 static struct dfc_keptq dfc_keptq;
 
+/*
+ * PREREQUISITE: giant_lock
+ */
 void
-removal_pendingq_enqueue(struct dead_file_copy *dfc)
+dead_file_copy_schedule_removal(struct dead_file_copy *dfc)
 {
 	struct netsendq *qhost =
 	    abstract_host_get_sendq(dfc->qentry.abhost);
+	static const char diag[] = "dead_file_copy_schedule_removal";
 
+	if ((dfc->dfc_flags & DFC_FLAG_KEPT) != 0) {
+		gfarm_mutex_lock(&dfc_keptq.mutex, diag, "lock");
+		dfc->dfc_flags &= ~(DFC_FLAG_KEPT|DFC_FLAG_KEPT_HARD);
+		GFARM_HCIRCLEQ_REMOVE(&dfc->qentry, workq_entries);
+		gfarm_mutex_unlock(&dfc_keptq.mutex, diag, "unlock");
+	}
 	/* should success always, because of NETSENDQ_FLAG_QUEUEABLE_IF_DOWN */
 	(void)netsendq_add_entry(qhost, &dfc->qentry, 0);
 }
@@ -91,7 +103,9 @@ handle_removal_result(struct netsendq_entry *qentryp)
 		    (long long)dfc->inum, (long long)dfc->igen,
 		    abstract_host_get_name(dfc->qentry.abhost),
 		    gfarm_error_string(dfc->qentry.result));
-		removal_pendingq_enqueue(dfc);
+		giant_lock();
+		dead_file_copy_schedule_removal(dfc);
+		giant_unlock();
 	}
 }
 
@@ -161,10 +175,11 @@ dead_file_copy_host_becomes_up(struct host *host)
 	/* giant_lock prevents dfc from being freed */
 	GFARM_HCIRCLEQ_FOREACH_SAFE(qe, dfc_keptq.q, workq_entries, tmp) {
 		dfc = (struct dead_file_copy *)qe;
-		if (dead_file_copy_is_removable(dfc)) {
-			dfc->is_kept = 0;
+		if ((dfc->dfc_flags & DFC_FLAG_KEPT_HARD) == 0 &&
+		    dead_file_copy_is_removable(dfc)) {
+			dfc->dfc_flags &= ~DFC_FLAG_KEPT;
 			GFARM_HCIRCLEQ_REMOVE(qe, workq_entries);
-			removal_pendingq_enqueue(dfc);
+			dead_file_copy_schedule_removal(dfc);
 		}
 	}
 
@@ -200,9 +215,13 @@ dead_file_copy_host_removed(struct host *host)
 		dfc = (struct dead_file_copy *)qe;
 		if (abstract_host_to_host(dfc->qentry.abhost) != host)
 			continue;
+		if ((dfc->dfc_flags & DFC_FLAG_KEPT_HARD) != 0) {
+			/* still refered by struct file_replication */
+			continue;
+		}
 
+		dfc->dfc_flags &= ~DFC_FLAG_KEPT;
 		GFARM_HCIRCLEQ_REMOVE(qe, workq_entries);
-		dfc->is_kept = 0;
 		/*
 		 * to prevent functions which acquire dfc_kept.mutex
 		 * from sleeping
@@ -233,11 +252,14 @@ dead_file_copy_inode_status_changed(struct dead_file_copy_list *same_inode_list)
 	static const char diag[] = "dead_file_copy_inode_status_changed";
 
 	GFARM_HCIRCLEQ_FOREACH(dfc, same_inode_list->list, same_inode_copies) {
-		if (dfc->is_kept) {
+		/* kept && !kept_hard && removable ? */
+		if ((dfc->dfc_flags & (DFC_FLAG_KEPT|DFC_FLAG_KEPT_HARD)) ==
+		    DFC_FLAG_KEPT && dead_file_copy_is_removable(dfc)) {
 			gfarm_mutex_lock(&dfc_keptq.mutex, diag, "lock");
-			GFARM_HCIRCLEQ_REMOVE(dfc, same_inode_copies);
-			dfc->is_kept = 0;
+			dfc->dfc_flags &= ~DFC_FLAG_KEPT;
+			GFARM_HCIRCLEQ_REMOVE(&dfc->qentry, workq_entries);
 			gfarm_mutex_unlock(&dfc_keptq.mutex, diag, "unlock");
+			dead_file_copy_schedule_removal(dfc);
 		}
 	}
 }
@@ -248,26 +270,52 @@ dead_file_copy_inode_status_changed(struct dead_file_copy_list *same_inode_list)
  * LOCKS: XXX
  * SLEEPS: XXX
  */
-void
-dead_file_copy_mark_kept(struct dead_file_copy *dfc)
+static void
+dead_file_copy_mark_kept_internal(struct dead_file_copy *dfc, int hard)
 {
 	static const char diag[] = "dead_file_copy_mark_kept";
 
 	/* sanity check */
-	if (dfc->is_kept) {
+	if ((dfc->dfc_flags & DFC_FLAG_KEPT) != 0) {
 		gflog_error(GFARM_MSG_UNFIXED,
-		    "dead copy %lld:%lld host %s: already kept",
+		    "dead copy %lld:%lld host %s: already kept %d->%d",
 		    (long long)dfc->inum, (long long)dfc->igen,
-		    abstract_host_get_name(dfc->qentry.abhost));
+		    abstract_host_get_name(dfc->qentry.abhost),
+		    (dfc->dfc_flags & DFC_FLAG_KEPT_HARD) != 0, hard);
+		if (((dfc->dfc_flags & DFC_FLAG_KEPT_HARD) != 0) != hard)
+			dfc->dfc_flags ^= DFC_FLAG_KEPT_HARD;
 		return;
 	}
 
 	gfarm_mutex_lock(&dfc_keptq.mutex, diag, "lock");
 
+	dfc->dfc_flags |= DFC_FLAG_KEPT;
+	if (hard)
+		dfc->dfc_flags |= DFC_FLAG_KEPT_HARD;
 	GFARM_HCIRCLEQ_INSERT_TAIL(dfc_keptq.q, &dfc->qentry, workq_entries);
-	dfc->is_kept = 1;
 
 	gfarm_mutex_unlock(&dfc_keptq.mutex, diag, "unlock");
+}
+
+void
+dead_file_copy_mark_kept_hard(struct dead_file_copy *dfc)
+{
+	dead_file_copy_mark_kept_internal(dfc, 1);
+}
+
+static void
+dead_file_copy_mark_kept_soft(struct dead_file_copy *dfc)
+{
+	dead_file_copy_mark_kept_internal(dfc, 0);
+}
+
+/*
+ * PREREQUISITE: giant_lock
+ */
+void
+dead_file_copy_change_kept_soft(struct dead_file_copy *dfc)
+{
+	dfc->dfc_flags &= ~DFC_FLAG_KEPT_HARD;
 }
 
 /*
@@ -472,7 +520,7 @@ dead_file_copy_alloc(gfarm_ino_t inum, gfarm_uint64_t igen, struct host *host)
 	dfc->qentry.abhost = host_to_abstract_host(host);
 	dfc->inum = inum;
 	dfc->igen = igen;
-	dfc->is_kept = 0;
+	dfc->dfc_flags = 0;
 
 	return (dfc);
 }
@@ -521,7 +569,7 @@ dead_file_copy_list_free_check(struct dead_file_copy_list *same_inode_list)
 }
 
 /*
- * PREREQUISITE: nothing (XXX host_name()?)
+ * PREREQUISITE: giant_lock
  * LOCKS: dfc_keptq.mutex, dbq.mutex
  * SLEEPS: yes (dbq.mutex)
  */
@@ -583,8 +631,10 @@ dead_file_copy_free(struct dead_file_copy *dfc)
 	static const char diag[] = "dead_file_copy_free";
 
 	gfarm_mutex_lock(&dfc_keptq.mutex, diag, "keptq lock");
-	if (dfc->is_kept)
+	if ((dfc->dfc_flags & DFC_FLAG_KEPT) != 0) {
+		dfc->dfc_flags &= ~(DFC_FLAG_KEPT|DFC_FLAG_KEPT_HARD);
 		GFARM_HCIRCLEQ_REMOVE(&dfc->qentry, workq_entries);
+	}
 	gfarm_mutex_unlock(&dfc_keptq.mutex, diag, "keptq unlock");
 
 	e = db_deadfilecopy_remove(dfc->inum, dfc->igen,
@@ -638,7 +688,9 @@ dead_file_copy_add_one(void *closure,
 	if (dfc != NULL) {
 		inode_dead_file_copy_added(inum, igen, host, dfc);
 		if (dead_file_copy_is_removable(dfc))
-			removal_pendingq_enqueue(dfc);
+			dead_file_copy_schedule_removal(dfc);
+		else
+			dead_file_copy_mark_kept_soft(dfc);
 	}
 
 }

server/gfmd/dead_file_copy.h
@@ -16,14 +16,15 @@ void dead_file_copy_list_add(struct dead_file_copy_list **,
 	struct dead_file_copy *);
 int dead_file_copy_list_free_check(struct dead_file_copy_list *);
 
-void removal_pendingq_enqueue(struct dead_file_copy *);
+void dead_file_copy_schedule_removal(struct dead_file_copy *);
 
 void dead_file_copy_host_becomes_up(struct host *);
 void dead_file_copy_host_removed(struct host *);
 struct dead_file_copy_list;
 void dead_file_copy_inode_status_changed(struct dead_file_copy_list *);
 
-void dead_file_copy_mark_kept(struct dead_file_copy *);
+void dead_file_copy_mark_kept_hard(struct dead_file_copy *);
+void dead_file_copy_change_kept_soft(struct dead_file_copy *);
 
 int dead_file_copy_count_by_inode(struct dead_file_copy_list *,
 	gfarm_uint64_t, int);

server/gfmd/inode.c
@@ -947,7 +947,8 @@ replicate_on_new_node_done:
 				 * Give up the replication and remove
 				 * the old one
 				 */
-				removal_pendingq_enqueue(deferred_cleanup);
+				dead_file_copy_schedule_removal(
+				    deferred_cleanup);
 			}
 		}
 
@@ -1709,6 +1710,8 @@ inode_replication_start(struct inode *inode)
 	assert(ia != NULL);
 	if (ia->u.f.rstate != NULL)
 		file_replication_start(ia->u.f.rstate, inode->i_gen);
+	else if (inode->dead_copies != NULL)
+		dead_file_copy_inode_status_changed(inode->dead_copies);
 }
 
 gfarm_error_t
@@ -3627,7 +3630,7 @@ inode_replicated(struct file_replication *fr,
 		    file_replication_get_dst(fr), file_replication_get_gen(fr),
 		    &dfc);
 		if (e == GFARM_ERR_NO_ERROR) {
-			removal_pendingq_enqueue(dfc);
+			dead_file_copy_schedule_removal(dfc);
 		} else if (e == GFARM_ERR_NO_SUCH_OBJECT) {
 			gflog_info(GFARM_MSG_1002433,
 			    "cannot remove an incomplete replica "
@@ -3657,9 +3660,10 @@ inode_replicated(struct file_replication *fr,
 		 */
 		if (src_errcode == GFARM_ERR_NO_ERROR &&
 		    dead_file_copy_is_removable(dfc))
-			removal_pendingq_enqueue(dfc);
-		else
-			dead_file_copy_mark_kept(dfc);
+			dead_file_copy_schedule_removal(dfc);
+		else /* change kept_hard -> kept_soft */
+			dead_file_copy_change_kept_soft(dfc);
+		/* otherwise this dfc is already kept */
 	} else if (e == GFARM_ERR_NO_ERROR) {
 		/* try to sweep kept queue */
 		if (inode->dead_copies != NULL)
@@ -3829,10 +3833,11 @@ remove_replica_entity(struct inode *inode, gfarm_int64_t gen,
 		    "remove_replica_entity(%lld, %lld, %s): no memory",
 		    (unsigned long long)inode->i_number,
 		    (unsigned long long)gen, host_name(spool_host));
-	} else if (deferred_cleanupp == NULL)
-		removal_pendingq_enqueue(dfc);
-	else {
-		dead_file_copy_mark_kept(dfc); /* prevent this from removed */
+	} else if (deferred_cleanupp == NULL) {
+		dead_file_copy_schedule_removal(dfc);
+	} else {
+		/* prevent this from removed */
+		dead_file_copy_mark_kept_hard(dfc);
 		*deferred_cleanupp = dfc;
 	}
 