Cloudxtreme/gfarm

commit 5a7657a11d62a4567c88d8474af6dcc1f777f3fe

Fix a bug that gfmd crashes when an XML extended attribute name written in non-UTF-8 string is given. (#389)
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6531 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfutil/gfutil.h
@@ -37,3 +37,8 @@ int gfarm_timeval_is_expired(const struct timeval *);
 /* random */
 
 long gfarm_random(void);
+
+/* utf8 */
+
+int gfarm_utf8_validate_string(const char *);
+ 

lib/libgfarm/gfutil/utf8.c
@@ -1,0 +1,61 @@
+/*
+ * Return 1 if 'string' is a valid UTF-8 string, 0 otherwise.
+ */
+int
+gfarm_utf8_validate_string(const char *string)
+{
+	const unsigned char *s = (const unsigned char *)string;
+	unsigned long codepoint;
+	unsigned long min_codepoint;
+	unsigned long max_codepoint;
+	int nfollows;
+
+	while (*s != '\0') {
+		if (*s <= 0x7f) {
+			nfollows = 0;
+			min_codepoint = 0x00;
+			max_codepoint = 0x7f;
+			codepoint = *s & 0x7f;
+		} else if (*s <= 0xbf) {
+			return 0;
+		} else if (*s <= 0xdf) {
+			nfollows = 1;
+			min_codepoint = 0x0080;
+			max_codepoint = 0x07ff;
+			codepoint = *s & 0x1f;
+		} else if (*s <= 0xef) {
+			nfollows = 2;
+			min_codepoint = 0x0800;
+			max_codepoint = 0xffff;
+			codepoint = *s & 0x0f;
+		} else if (*s <= 0xf7) {
+			nfollows = 3;
+			min_codepoint = 0x010000;
+			max_codepoint = 0x10ffff;
+			codepoint = *s & 0x07;
+		} else {
+			return 0;
+		}
+
+		s++;
+		while (nfollows > 0) {
+			if (0x80 <= *s && *s <= 0xbf)
+				codepoint = (codepoint << 6) | (*s & 0x3f);
+			else
+				return 0;
+			s++;
+			nfollows--;
+		}
+
+		if (codepoint < min_codepoint || codepoint > max_codepoint)
+			return 0;
+
+		/*
+		 * Surrogates are not allowed.
+		 */
+		if (0xd800 <= codepoint && codepoint <= 0xdfff)
+			return 0;
+	}
+
+	return 1;
+}

server/gfmd/xattr.c
@@ -196,6 +196,9 @@ isvalid_attrname(const char *attrname)
 {
 	int namelen = strlen(attrname);
 
+	if (!gfarm_utf8_validate_string(attrname))
+		return 0;
+
 	return ((0 < namelen) && (namelen <= MAX_XATTR_NAME_LEN));
 }
 
@@ -211,6 +214,13 @@ setxattr(int xmlMode, struct inode *inode,
 	if (!isvalid_attrname(attrname)) {
 		gflog_debug(GFARM_MSG_1002066,
 			"argument 'attrname' is invalid");
+		return GFARM_ERR_INVALID_ARGUMENT;
+	}
+
+	if (!gfarm_utf8_validate_string(*valuep)) {
+		e = GFARM_ERR_INVALID_ARGUMENT;
+		gflog_debug(GFARM_MSG_UNFIXED,
+		    "argument '*valuep' is not a valid UTF-8 string");
 		return GFARM_ERR_INVALID_ARGUMENT;
 	}
 

lib/libgfarm/gfutil/Makefile
@@ -26,7 +26,8 @@ SRCS =	alloc.c \
 	thrbarrier.c \
 	thrsubr.c \
 	timer.c \
-	timeval.c
+	timeval.c \
+	utf8.c
 OBJS =	alloc.lo \
 	assert.lo \
 	backtrace.lo \
@@ -44,7 +45,8 @@ OBJS =	alloc.lo \
 	thrbarrier.lo \
 	thrsubr.lo \
 	timer.lo \
-	timeval.lo
+	timeval.lo \
+	utf8.lo
 
 CFLAGS = $(pthread_includes) $(COMMON_CFLAGS) -I$(GFUTIL_SRCDIR)
 

regress/Makefile
@@ -10,6 +10,7 @@ CHECK_ENV=	LANG=C PATH=$(prefix)/bin:$$PATH
 
 # subdirectories which have to be built
 SUBDIRS=	\
+	lib/libgfarm/gfutil/utf8 \
 	lib/libgfarm/gfarm/gfarm_error_range_alloc \
 	lib/libgfarm/gfarm/gfarm_error_to_errno \
 	lib/libgfarm/gfarm/gfs_dir_test \

regress/gftool/gfxattr/gfxattr-xml-non-utf8-value.sh
@@ -1,0 +1,56 @@
+#!/bin/sh
+
+. ./regress.conf
+
+dir=${gftmp}
+
+attr_src=${testbin}/gfxattr-xml-non-utf8-value.src
+attr_log=${localtmp}.log
+
+attrname="user.longvalue"
+
+setup() {
+    gfmkdir ${dir}
+    gfxattr -r -x ${dir} ${attrname}
+    rm -f ${attr_log}
+}
+
+cleanup() {
+    rm -f ${attr_log}
+    gfxattr -r -x ${dir} ${attrname}
+    gfrmdir ${dir}
+}
+
+# is XML attr supported?
+if gfxattr -x -g / test 2>&1 |
+        egrep "^gfxattr: (unknown|invalid) option -- ('x'|x)" >/dev/null
+then
+        exit $exit_unsupported
+fi
+
+trap 'cleanup; exit $exit_trap' $trap_sigs
+
+setup
+
+gfxattr -s -c -x -f ${attr_src} ${dir} ${attrname} >${attr_log} 2>&1
+if test $? -eq 0; then
+    cleanup
+    exit $exit_fail
+fi
+
+if grep "invalid argument" ${attr_log} >/dev/null 2>&1; then
+    true
+else
+    cleanup
+    exit $exit_fail
+fi
+
+gfxattr -l -x ${dir}
+if test $? -ne 0; then
+    cleanup
+    exit $exit_fail
+fi
+
+cleanup
+
+exit $exit_pass

regress/gftool/gfxattr/gfxattr-xml-non-utf8-value.src
@@ -1,0 +1,1 @@
+あいうえお

regress/lib/libgfarm/gfutil/utf8/Makefile
@@ -1,0 +1,23 @@
+top_builddir = ../../../../..
+top_srcdir = $(top_builddir)
+srcdir = .
+
+include $(top_srcdir)/makes/var.mk
+
+PROGRAM = utf8_test
+SRCS = $(PROGRAM).c
+OBJS = $(PROGRAM).o
+CFLAGS = $(COMMON_CFLAGS) -I$(GFUTIL_SRCDIR)
+LDLIBS = $(COMMON_LDLIBS) $(GFARMLIB) $(LIBS)
+DEPLIBS = $(DEPGFARMLIB)
+
+all: $(PROGRAM)
+
+include $(top_srcdir)/makes/prog.mk
+
+###
+
+$(OBJS): $(DEPGFARMINC)
+
+post-clean-hook:
+	$(RM) -f utf8_test.log

commit 1784f2a784650af488311c84f88a1d8393c6d06e

Fix a bug in gfmd caused by r6531 (for ticket #389) that the changeset makes gfmd always return 'illegal byte sequence' for 'gfxattr -g'.
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6536 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfutil/gfutil.h
@@ -41,4 +41,4 @@ long gfarm_random(void);
 /* utf8 */
 
 int gfarm_utf8_validate_string(const char *);
- 
+int gfarm_utf8_validate_sequences(const char *s, size_t);

lib/libgfarm/gfutil/utf8.c
@@ -1,16 +1,20 @@
+#include <stddef.h>
+#include <string.h>
+
 /*
- * Return 1 if 'string' is a valid UTF-8 string, 0 otherwise.
+ * Return 1 if 'sequences' consists of valid UTF-8 sequences.
  */
 int
-gfarm_utf8_validate_string(const char *string)
+gfarm_utf8_validate_sequences(const char *sequences, size_t len)
 {
-	const unsigned char *s = (const unsigned char *)string;
+	const unsigned char *s = (const unsigned char *)sequences;
 	unsigned long codepoint;
 	unsigned long min_codepoint;
 	unsigned long max_codepoint;
 	int nfollows;
 
-	while (*s != '\0') {
+	while (len > 0) {
+		len--;
 		if (*s <= 0x7f) {
 			nfollows = 0;
 			min_codepoint = 0x00;
@@ -37,7 +41,11 @@ gfarm_utf8_validate_string(const char *string)
 			return 0;
 		}
 
+		if (len < nfollows)
+			return 0;
+		len -= nfollows;
 		s++;
+
 		while (nfollows > 0) {
 			if (0x80 <= *s && *s <= 0xbf)
 				codepoint = (codepoint << 6) | (*s & 0x3f);
@@ -58,4 +66,13 @@ gfarm_utf8_validate_string(const char *string)
 	}
 
 	return 1;
+}
+
+/*
+ * Return 1 if 'string' is a valid UTF-8 string, 0 otherwise.
+ */
+int
+gfarm_utf8_validate_string(const char *string)
+{
+	return gfarm_utf8_validate_sequences(string, strlen(string));
 }

server/gfmd/xattr.c
@@ -217,7 +217,7 @@ setxattr(int xmlMode, struct inode *inode,
 		return GFARM_ERR_INVALID_ARGUMENT;
 	}
 
-	if (!gfarm_utf8_validate_string(*valuep)) {
+	if (!gfarm_utf8_validate_sequences(*valuep, size)) {
 		e = GFARM_ERR_INVALID_ARGUMENT;
 		gflog_debug(GFARM_MSG_UNFIXED,
 		    "argument '*valuep' is not a valid UTF-8 string");