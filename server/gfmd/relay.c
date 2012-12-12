Cloudxtreme/gfarm

commit a6611faa5b48cba51a5743fd2bed0a9ddfe64e41

a sequel to r7483: fix one of race conditions of r6751 (for #287 - inter-gfmd RPC forwarding): need to check that this reply is really sent to the peer which sent the request
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7484 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/relay.c
@@ -917,6 +917,14 @@ request_reply_dynarg_master(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 		return (e);
 	}
 
+	if (mhpeer != peer) {
+		gflog_error(GFARM_MSG_UNFIXED,
+		    "gfmd_channel(%s): peer switch during rpc relay",
+		    abstract_host_get_name(ah));
+		e = GFARM_ERR_CONNECTION_ABORTED;
+		goto unlock_sender;
+	}
+
 	if ((e = gfp_xdr_send_async_result_header(conn, xid, size))
 	    != GFARM_ERR_NO_ERROR) {
 		gflog_debug(GFARM_ERR_NO_ERROR,

commit 04e10b7d28cbc22bbe80ab40aefb57a55f04fa9b

fix the following problems:  * a bug which broke #287 (inter-gfmd RPC forwarding).[[BR]] this bug was introduced in r7484 and r7487.  * error code handling of gfm_server_put_wrapped_vreply()
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7490 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/relay.c
@@ -868,7 +868,7 @@ request_reply_dynarg_master(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 	gfarm_uint64_t seqnum;
 	gfarm_uint64_t flags;
 	struct abstract_host *ah;
-	struct peer *mhpeer = NULL;
+	struct peer *slave_mhpeer, *mhpeer = NULL;
 	struct gfarm_thr_statewait *statewait;
 	struct remote_peer *remote_peer;
 	static const char relay_diag[] = "request_reply_dynarg_master";
@@ -885,7 +885,8 @@ request_reply_dynarg_master(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 		return (e);
 	}
 
-	ah = mdhost_to_abstract_host(peer_get_mdhost(peer));
+	slave_mhpeer = peer_get_parent(peer);
+	ah = mdhost_to_abstract_host(peer_get_mdhost(slave_mhpeer));
 	remote_peer = peer_to_remote_peer(peer);
 	statewait = remote_peer_get_statewait(remote_peer);
 	gfarm_thr_statewait_signal(statewait, e, diag);
@@ -917,7 +918,7 @@ request_reply_dynarg_master(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 		return (e);
 	}
 
-	if (mhpeer != peer) {
+	if (mhpeer != slave_mhpeer) {
 		gflog_error(GFARM_MSG_UNFIXED,
 		    "gfmd_channel(%s): peer switch during rpc relay reply",
 		    abstract_host_get_name(ah));
@@ -1286,7 +1287,7 @@ gfm_server_relay_put_reply(struct peer *peer, gfp_xdr_xid_t xid,
 	gfarm_error_t ecode, const char *format, ...)
 {
 	struct abstract_host *ah;
-	struct peer *mhpeer = NULL;
+	struct peer *slave_mhpeer, *mhpeer = NULL;
 	va_list ap;
 	gfarm_error_t e;
 	gfarm_uint64_t seqnum;
@@ -1330,13 +1331,14 @@ gfm_server_relay_put_reply(struct peer *peer, gfp_xdr_xid_t xid,
 		/*
 		 * This gfmd is master and the request comes from a slave.
 		 */
-		ah = mdhost_to_abstract_host(peer_get_mdhost(peer));
+		slave_mhpeer = peer_get_parent(peer);
+		ah = mdhost_to_abstract_host(peer_get_mdhost(slave_mhpeer));
 		if ((e = abstract_host_sender_lock(ah, &mhpeer, diag))
 		    != GFARM_ERR_NO_ERROR) {
 			gflog_debug(GFARM_MSG_UNFIXED,
 			    "%s: %s (abstract_host_sender_lock): %s",
 			    diag, relay_diag, gfarm_error_string(e));
-		} else if (mhpeer != peer) {
+		} else if (mhpeer != slave_mhpeer) {
 			abstract_host_sender_unlock(ah, mhpeer, diag);
 			gflog_error(GFARM_MSG_UNFIXED, "gfmd_channel(%s): "
 			    "peer switch during rpc relay reply",

server/gfmd/rpcsubr.c
@@ -96,22 +96,24 @@ gfm_server_put_wrapped_vreply0(struct peer *peer, gfp_xdr_xid_t xid,
 {
 	gfarm_error_t e;
 	struct gfp_xdr *client = peer_get_conn(peer);
+	struct peer *slave_mhpeer;
 
 	if (debug_mode)
 		gflog_info(GFARM_MSG_1000229,
 		    "<%s> sending reply: %d", diag, (int)ecode);
 
 	if (sizep != NULL) {
-		if (peer_get_parent(peer) != NULL) /* remote_peer from slave */
-			peer = peer_get_parent(peer);
+		slave_mhpeer = peer_get_parent(peer);
 		if (do_lock) {
 			e = async_server_vput_wrapped_reply(
-			    peer_get_abstract_host(peer), peer, xid, xdr_vsend,
+			    peer_get_abstract_host(slave_mhpeer), slave_mhpeer,
+			    xid, xdr_vsend,
 			    diag,
 			    ecode, wrapping_format, wrapping_app, format, app);
 		} else {
 			e = async_server_vput_wrapped_reply_unlocked(
-			    peer_get_abstract_host(peer), xid, xdr_vsend,
+			    peer_get_abstract_host(slave_mhpeer),
+			    xid, xdr_vsend,
 			    diag,
 			    ecode, wrapping_format, wrapping_app, format, app);
 		}