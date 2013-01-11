Cloudxtreme/gfarm

commit fc4ca36dbd0fc2751786fb3e3e634b9837c1522d

fix #457 - automatic replication does not work, if multiple simultaneous write-open calls are made, but last closing process doesn't actually write the file
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@6701 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -159,6 +159,7 @@ struct inode_activity {
 
 			struct gfarm_timespec last_update;
 			int writers, spool_writers;
+			int replication_pending;
 
 			struct inode_replication_state *rstate;
 		} f;
@@ -550,6 +551,7 @@ inode_activity_alloc(void)
 
 	ia->u.f.writers = 0;
 	ia->u.f.spool_writers = 0;
+	ia->u.f.replication_pending = 0;
 
 	ia->u.f.event_waiters = NULL;
 	ia->u.f.event_source = NULL;
@@ -866,11 +868,14 @@ make_replicas_except(struct inode *inode, struct host *spool_host,
 	} else if (n_exceptions - being_removed < desired_replica_number) {
 
 		gflog_debug(GFARM_MSG_UNFIXED,
-		    "update_replicas(): about to schedule "
-		    "ncopy-based replication for inode %lld:%lld@%s.",
+		    "%s: about to schedule "
+		    "ncopy-based replication for inode %lld:%lld@%s. "
+		    "number = %d (= %d - %d + %d)", diag,
 		    (long long)inode_get_number(inode),
 		    (long long)inode_get_gen(inode),
-		    host_name(spool_host));
+		    host_name(spool_host),
+		    desired_replica_number - n_exceptions + being_removed,
+		    desired_replica_number, n_exceptions, being_removed);
 
 		inode_schedule_replication_from_all(
 		    inode, spool_host, &n_exceptions, exceptions,
@@ -3152,6 +3157,26 @@ inode_del_ref_spool_writers(struct inode *inode)
 	--ia->u.f.spool_writers;
 }
 
+void
+inode_check_pending_replication(struct file_opening *fo)
+{
+	struct inode *inode = fo->inode;
+	struct host *spool_host = fo->u.f.spool_host;
+	struct inode_activity *ia = inode->u.c.activity;
+
+	assert(spool_host != NULL && ia != NULL);
+
+	if (host_supports_async_protocols(spool_host) &&
+	    ia->u.f.spool_writers == 0 && ia->u.f.replication_pending) {
+		ia->u.f.replication_pending = 0;
+		make_replicas_except(inode, spool_host,
+		    fo->u.f.desired_replica_number, fo->u.f.repattr,
+		    inode->u.c.s.f.copies);
+		if (!inode_new_generation_is_pending(inode))
+			inode_replication_start(inode);
+	}
+}
+
 /*
  * returns TRUE, if generation number is updated.
  *
@@ -3211,8 +3236,12 @@ inode_file_update_common(struct inode *inode, gfarm_off_t size,
 		/* XXX provide an option not to start replication here? */
 
 		/* if there is no other writing process */
-		if (ia == NULL || ia->u.f.spool_writers == 0)
+		if (ia == NULL || ia->u.f.spool_writers == 0) {
 			start_replication = 1;
+			ia->u.f.replication_pending = 0;
+		} else {
+			ia->u.f.replication_pending = 1;
+		}
 	}
 
 	update_replicas(inode, spool_host, old_gen,

regress/lib/libgfarm/gfarm/gfs_pio_create/autoreplica4_reader_last.sh
@@ -63,7 +63,7 @@ do
 	if [ $WAIT_TIME -gt $WAIT_TIME_LIMIT ]; then
 		echo replication timeout
 		clean
-		exit $exit_xfail
+		exit $exit_fail
 	fi
 	sleep 1
 done

server/gfmd/process.c
@@ -865,8 +865,10 @@ process_close_file(struct process *process, struct peer *peer, int fd,
 			return (GFARM_ERR_OPERATION_NOT_PERMITTED);
 		}
 		/* i.e. REOPENed file, and I am a gfsd. */
-		if ((accmode_to_op(fo->flag) & GFS_W_OK) != 0)
+		if ((accmode_to_op(fo->flag) & GFS_W_OK) != 0) {
 			inode_del_ref_spool_writers(fo->inode);
+			inode_check_pending_replication(fo);
+		}
 		if (fo->opener != NULL) {
 			/*
 			 * a gfsd is closing a REOPENed file,
@@ -921,8 +923,10 @@ process_close_file_read(struct process *process, struct peer *peer, int fd,
 		return (GFARM_ERR_OPERATION_NOT_PERMITTED);
 	}
 
-	if ((accmode_to_op(fo->flag) & GFS_W_OK) != 0)
+	if ((accmode_to_op(fo->flag) & GFS_W_OK) != 0) {
 		inode_del_ref_spool_writers(fo->inode);
+		inode_check_pending_replication(fo);
+	}
 	if (fo->opener != peer && fo->opener != NULL) {
 		/* closing REOPENed file, but the client is still opening */
 		fo->u.f.spool_opener = NULL;

commit 00fd168cca8663b59fe967eeded82dbb9b360efe

fix gfmd crash at gfmd failover or gfsd reconnection (comment:10:ticket:457), which was introduced in r6701 during the fix for #457 - automatic replication does not work, if multiple simultaneous write-open calls are made, but last closing process doesn't actually write the file.
problem found by Kiyoshi Imai-san.

NOTE: this problem only exists on the trunk.


git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7595 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/inode.c
@@ -3302,6 +3302,7 @@ inode_file_update_common(struct inode *inode, gfarm_off_t size,
 	gfarm_int64_t *old_genp, gfarm_int64_t *new_genp,
 	char **trace_logp)
 {
+	/* ia may be NULL, if GFM_PROTO_FHCLOSE_READ/WRITE */
 	struct inode_activity *ia = inode->u.c.activity;
 	gfarm_int64_t old_gen;
 	int generation_updated = 0;
@@ -3349,7 +3350,9 @@ inode_file_update_common(struct inode *inode, gfarm_off_t size,
 		/* XXX provide an option not to start replication here? */
 
 		/* if there is no other writing process */
-		if (ia == NULL || ia->u.f.spool_writers == 0) {
+		if (ia == NULL) {
+			start_replication = 1;
+		} else if (ia->u.f.spool_writers == 0) {
 			start_replication = 1;
 			ia->u.f.replication_pending = 0;
 		} else {