Cloudxtreme/gfarm

commit 2b189c0e909532e86984546fe04c70e33fe980b3

revised fix for the problem 2 in #408 - incomplete replica is left, i.e. chicken-or-egg problem which prevented peer_free() from being called. problem found by Kiyoshi Imai-san.
We don't have a plan to apply this change to the main trunk,
because the trunk doesn't have the problem 2, and this patch is somewhat ugly.


git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@6694 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/host.c
@@ -543,6 +543,22 @@ host_put_peer(struct host *h, struct peer *peer)
 	abstract_host_put_peer(&h->ah, peer);
 }
 
+static struct peer *
+host_get_peer_for_replication(struct host *h)
+{
+	struct peer *peer = host_get_peer(h);
+
+	peer_add_ref_for_replication(peer);
+	host_put_peer(h, peer);
+	return (peer);
+}
+
+void
+host_put_peer_for_replication(struct host *h, struct peer *peer)
+{
+	peer_del_ref_for_replication(peer);
+}
+
 int
 host_status_callout_retry(struct host *host)
 {
@@ -777,7 +793,8 @@ host_free(struct host *h)
 gfarm_error_t
 host_replicating_new(struct host *dst, struct file_replicating **frp)
 {
-	struct peer *peer = host_get_peer(dst); /* increment refcount */
+	/* increment replication_refcount */
+	struct peer *peer = host_get_peer_for_replication(dst);
 
 	if (peer == NULL)
 		return (GFARM_ERR_NO_ROUTE_TO_HOST);

server/gfmd/host.h
@@ -26,6 +26,7 @@ void host_disconnect_request(struct host *, struct peer *);
 struct callout *host_status_callout(struct host *);
 struct peer *host_get_peer(struct host *);
 void host_put_peer(struct host *, struct peer *);
+void host_put_peer_for_replication(struct host *, struct peer *);
 
 char *host_name(struct host *);
 int host_port(struct host *);

server/gfmd/peer.c
@@ -129,6 +129,7 @@ struct cookie {
 struct peer {
 	struct peer *next_close;
 	int refcount;
+	int replication_refcount;
 
 	struct gfp_xdr *conn;
 	gfp_xdr_async_peer_t async; /* used by {back|gfmd}_channel */
@@ -214,7 +215,8 @@ peer_replicating_new(struct peer *peer, struct host *dst,
 
 	GFARM_MALLOC(fr);
 	if (fr == NULL) {
-		host_put_peer(dst, peer); /* decrement refcount */
+		/* decrement replication_refcount */
+		host_put_peer_for_replication(dst, peer);
 		return (GFARM_ERR_NO_MEMORY);
 	}
 
@@ -241,7 +243,8 @@ peer_replicating_new(struct peer *peer, struct host *dst,
 	gfarm_mutex_unlock(&peer->replication_mutex, diag, replication_diag);
 
 	if (fr == NULL) {
-		host_put_peer(dst, peer); /* decrement refcount */
+		/* decrement replication_refcount */
+		host_put_peer_for_replication(dst, peer);
 		return (GFARM_ERR_RESOURCE_TEMPORARILY_UNAVAILABLE);
 	}
 	*frp = fr;
@@ -262,7 +265,8 @@ peer_replicating_free(struct file_replicating *fr)
 	fr->next_inode->prev_inode = fr->prev_inode;
 	gfarm_mutex_unlock(&peer->replication_mutex, diag, replication_diag);
 
-	host_put_peer(fr->dst, fr->peer); /* decrement refcount */
+	/* decrement replication_refcount */
+	host_put_peer_for_replication(fr->dst, fr->peer);
 
 	free(fr);
 }
@@ -370,10 +374,46 @@ peer_del_ref(struct peer *peer)
 	return (referenced);
 }
 
+void
+peer_add_ref_for_replication(struct peer *peer)
+{
+	static const char diag[] = "peer_add_ref_for_replication";
+
+	gfarm_mutex_lock(&peer_closing_queue.mutex,
+	    diag, "peer_closing_queue");
+	++peer->replication_refcount;
+	gfarm_mutex_unlock(&peer_closing_queue.mutex,
+	    diag, "peer_closing_queue");
+}
+
+int
+peer_del_ref_for_replication(struct peer *peer)
+{
+	int referenced;
+	static const char diag[] = "peer_del_ref_for_replication";
+
+	gfarm_mutex_lock(&peer_closing_queue.mutex,
+	    diag, "peer_closing_queue");
+
+	if (--peer->replication_refcount > 0) {
+		referenced = 1;
+	} else {
+		referenced = 0;
+		gfarm_cond_signal(&peer_closing_queue.ready_to_close,
+		    diag, "ready to close");
+	}
+
+	gfarm_mutex_unlock(&peer_closing_queue.mutex,
+	    diag, "peer_closing_queue");
+
+	return (referenced);
+}
+
 void *
 peer_closer(void *arg)
 {
 	struct peer *peer, **prev;
+	int do_async_free;
 	static const char diag[] = "peer_closer";
 
 	gfarm_mutex_lock(&peer_closing_queue.mutex, diag,
@@ -385,11 +425,16 @@ peer_closer(void *arg)
 			    &peer_closing_queue.mutex,
 			    diag, "queue is not empty");
 
+		do_async_free = 0;
 		for (prev = &peer_closing_queue.head;
 		    (peer = *prev) != NULL; prev = &peer->next_close) {
-			if (peer->refcount == 0) {
-				if (!watcher_event_is_active(
-				    peer->readable_event)) {
+			if (peer->refcount == 0 &&
+			    !watcher_event_is_active(peer->readable_event)) {
+				if (peer->async != NULL &&
+				    peer_async_free != NULL) {
+					do_async_free = 1;
+					break;
+				} else if (peer->replication_refcount == 0) {
 					*prev = peer->next_close;
 					if (peer_closing_queue.tail ==
 					    &peer->next_close)
@@ -414,7 +459,18 @@ peer_closer(void *arg)
 		 * at least for now,
 		 * because only externalized descriptor needs the calls.
 		 */
-		peer_free(peer);
+		if (do_async_free) {
+			(*peer_async_free)(peer, peer->async);
+			peer->async = NULL;
+			/*
+			 * this peer cannot be freed yet,
+			 * wait until peer->replication_refcount becomes 0.
+			 * see the problem 2 of
+			 * https://sourceforge.net/apps/trac/gfarm/ticket/408
+			 */
+		} else {
+			peer_free(peer);
+		}
 		giant_unlock();
 
 		gfarm_mutex_lock(&peer_closing_queue.mutex,
@@ -731,11 +787,6 @@ peer_free(struct peer *peer)
 	char hostbuf[NI_MAXHOST];
 
 	gfarm_mutex_lock(&peer_table_mutex, diag, peer_table_diag);
-
-	if (peer->async != NULL && peer_async_free != NULL) {
-		(*peer_async_free)(peer, peer->async);
-		peer->async = NULL;
-	}
 
 	while (!GFARM_HCIRCLEQ_EMPTY(peer->cookies, hcircleq)) {
 		cookie = GFARM_HCIRCLEQ_FIRST(peer->cookies, hcircleq);

server/gfmd/peer.h
@@ -10,6 +10,8 @@ struct thread_pool *peer_watcher_get_thrpool(struct peer_watcher *);
 
 void peer_add_ref(struct peer *);
 int peer_del_ref(struct peer *);
+void peer_add_ref_for_replication(struct peer *);
+int peer_del_ref_for_replication(struct peer *);
 void peer_free_request(struct peer *);
 
 void peer_init(int);

commit 27827082369dbfc655ce81f75448e0f19d2345f1

fix #601 - gfmd crashes when gfmd-initiated replication is invoked against a filesystem node which is down: this problem was introduced in r6694, as a fix to #479 - incomplete replica is left due to a chicken-or-egg problem to maintain a reference count of struct peer
problem found by Kiyoshi Imai-san


git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.5@7700 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/host.c
@@ -565,8 +565,10 @@ host_get_peer_for_replication(struct host *h)
 {
 	struct peer *peer = host_get_peer(h);
 
-	peer_add_ref_for_replication(peer);
-	host_put_peer(h, peer);
+	if (peer != NULL) {
+		peer_add_ref_for_replication(peer);
+		host_put_peer(h, peer);
+	}
 	return (peer);
 }
 