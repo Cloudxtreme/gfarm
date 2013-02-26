Cloudxtreme/gfarm

commit ed1dedf26f5e021b8835d745540eba9348deeeee

merge r7775 from the main trunk: fix #625 - failover handling stops to work, if a metadata server of another gfarm filesystem is specified in a Gfarm URL
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@7776 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/filesystem.c
@@ -54,6 +54,8 @@ struct gfarm_filesystem_static {
 	struct gfarm_hash_table *ms2fs_hashtab;
 };
 
+#define MS2FS_HASHTAB_SIZE 17
+
 static void gfarm_filesystem_free(struct gfarm_filesystem *);
 
 gfarm_error_t
@@ -189,9 +191,9 @@ gfarm_filesystem_is_initialized(void)
 static gfarm_error_t
 gfarm_filesystem_ms2fs_hash_alloc(void)
 {
-#define MS2FS_HASHTAB_SIZE 17
-	if (staticp->ms2fs_hashtab)
-		gfarm_hash_table_free(staticp->ms2fs_hashtab);
+	if (staticp->ms2fs_hashtab != NULL)
+		return (GFARM_ERR_NO_ERROR);
+
 	staticp->ms2fs_hashtab = gfarm_hash_table_alloc(MS2FS_HASHTAB_SIZE,
 	    gfarm_filesystem_hash_index, gfarm_filesystem_hash_equal);
 	if (staticp->ms2fs_hashtab == NULL) {

commit 33edec4364bb74c92c4645a77e8dd1e55725084a

merge r7798 and r7799 from the main trunk: fix an assertion failure, described in comment:5:ticket:625, caused by r7776 - during the fix to #625 - failover handling stops to work, if a metadata server of another gfarm filesystem is specified in a Gfarm URL
this problem was found by Koji Ookuma-san (ookuma@).

while I am here, make gfarm_filesystem_set_metadb_server_list() work gracefully
even if memory shortage happens.


git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/branches/2.6@7800 4a5e1ece-6a47-475c-b646-731d720b41d0

lib/libgfarm/gfarm/filesystem.c
@@ -241,6 +241,20 @@ gfarm_filesystem_hash_enter(struct gfarm_filesystem *fs,
 	return (GFARM_ERR_NO_ERROR);
 }
 
+static void
+gfarm_filesystem_hash_purge(struct gfarm_filesystem *fs,
+	struct gfarm_metadb_server *ms)
+{
+	struct gfarm_filesystem_hash_id id;
+	int r;
+
+	id.hostname = gfarm_metadb_server_get_name(ms);
+	id.port = gfarm_metadb_server_get_port(ms);
+	r = gfarm_hash_purge(staticp->ms2fs_hashtab, &id, sizeof(id));
+	assert(r);
+	(void)r;
+}
+
 int
 gfarm_filesystem_is_default(struct gfarm_filesystem *fs)
 {
@@ -286,8 +300,23 @@ gfarm_filesystem_set_metadb_server_list(struct gfarm_filesystem *fs,
 	int i;
 	struct gfarm_metadb_server *ms, **servers;
 
-	for (i = 0; i < fs->nservers; ++i)
+	GFARM_MALLOC_ARRAY(servers, sizeof(void *) * n);
+	if (servers == NULL) {
+		gflog_debug(GFARM_MSG_1002570,
+		    "%s", gfarm_error_string(GFARM_ERR_NO_MEMORY));
+		return (GFARM_ERR_NO_MEMORY);
+	}
+	if ((e = gfarm_filesystem_ms2fs_hash_alloc()) != GFARM_ERR_NO_ERROR) {
+		gflog_debug(GFARM_MSG_1002568,
+		    "%s", gfarm_error_string(GFARM_ERR_NO_MEMORY));
+		free(servers);
+		return (e);
+	}
+
+	for (i = 0; i < fs->nservers; ++i) {
 		gfarm_metadb_server_set_is_removed(fs->servers[i], 1);
+		gfarm_filesystem_hash_purge(fs, fs->servers[i]);
+	}
 	for (i = 0; i < n; ++i)
 		gfarm_metadb_server_set_is_removed(metadb_servers[i], 0);
 	for (i = 0; i < fs->nservers; ++i) {
@@ -302,21 +331,10 @@ gfarm_filesystem_set_metadb_server_list(struct gfarm_filesystem *fs,
 	fs->servers = NULL;
 	fs->nservers = 0;
 
-	GFARM_MALLOC_ARRAY(servers, sizeof(void *) * n);
-	if (servers == NULL) {
-		gflog_debug(GFARM_MSG_1002570,
-		    "%s", gfarm_error_string(GFARM_ERR_NO_MEMORY));
-		return (GFARM_ERR_NO_MEMORY);
-	}
 	memcpy(servers, metadb_servers, sizeof(void *) * n);
 	fs->servers = servers;
 	fs->nservers = n;
 
-	if ((e = gfarm_filesystem_ms2fs_hash_alloc()) != GFARM_ERR_NO_ERROR) {
-		gflog_debug(GFARM_MSG_1002568,
-		    "%s", gfarm_error_string(GFARM_ERR_NO_MEMORY));
-		return (e);
-	}
 	for (i = 0; i < n; ++i) {
 		if ((e = gfarm_filesystem_hash_enter(fs, servers[i])) !=
 		    GFARM_ERR_NO_ERROR)