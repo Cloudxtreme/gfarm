Cloudxtreme/gfarm

commit 90ac0a2b166a24bb0431dca78160da0f3cd6bee8

This patch is for Ticket #287 (inter-gfmd RPC forwarding).
Fix a bug in relay.c:ensure_remote_peer().
It doesn't work correctly when a slave gfmd reconnects to master.
We re-implement the function as gfmdc.c:gfmdc_ensure_remote_peer().

This patch also includes some workarounds for backend protocol relay
support for #287.

git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7862 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/relay.c
@@ -447,33 +447,6 @@ slave_reply_relay(struct relayed_request *r, const char *format, va_list *app,
 	return (e);
 }
 
-static gfarm_error_t
-ensure_remote_peer(struct peer *peer, gfarm_int32_t command,
-	const char *diag, struct relayed_request **rp)
-{
-	gfarm_error_t e;
-	struct relayed_request *r;
-
-	r = relayed_request_new(command, diag);
-	if (r == NULL) {
-		e = GFARM_ERR_NO_MEMORY;
-		gflog_error(GFARM_MSG_UNFIXED,
-		    "%s", gfarm_error_string(e));
-		return (e);
-	}
-
-	if (!local_peer_get_remote_peer_allocated(peer_to_local_peer(peer)) &&
-	    (e = gfmdc_client_remote_peer_alloc(peer)) != GFARM_ERR_NO_ERROR) {
-		gflog_error(GFARM_MSG_UNFIXED,
-		    "%s: ensure_remote_peer: %s", diag, gfarm_error_string(e));
-		free(r);
-		return (e);
-	}
-
-	*rp = r;
-	return (GFARM_ERR_NO_ERROR);
-}
-
 static void
 gfm_server_start_get_request(struct peer *peer, const char *diag)
 {
@@ -560,29 +533,24 @@ gfm_server_relay_put_vrequest(struct peer *peer,
 {
 	gfarm_error_t e;
 	struct relayed_request *r;
-	struct mdhost *master_mh;
-	struct abstract_host *ah;
 	struct peer *mhpeer = NULL;
 
-	if ((e = ensure_remote_peer(peer, command, diag, &r))
+	r = relayed_request_new(command, diag);
+	if (r == NULL) {
+		e = GFARM_ERR_NO_MEMORY;
+		gflog_error(GFARM_MSG_UNFIXED,
+		    "%s", gfarm_error_string(e));
+		return (e);
+	}
+	*rp = r;
+
+	if ((e = gfmdc_ensure_remote_peer(peer, &mhpeer))
 	    != GFARM_ERR_NO_ERROR) {
-		gflog_debug(GFARM_MSG_UNFIXED,
-		    "%s: ensure_remote_peer(): %s",
-		    diag, gfarm_error_string(e));
+		gflog_debug(GFARM_MSG_UNFIXED, "%s", gfarm_error_string(e));
 		return (e);
 	}
 
-	master_mh = mdhost_lookup_master();
-	ah = mdhost_to_abstract_host(master_mh);
-	if ((e = abstract_host_sender_lock(ah, &mhpeer, diag))
-	    != GFARM_ERR_NO_ERROR) {
-		gflog_debug(GFARM_MSG_UNFIXED,
-		    "%s: abstract_host_sender_lock(): %s",
-		    diag, gfarm_error_string(e));
-		return (e);
-	}
-
-	e = slave_request_relay(master_mh, r, peer, command,
+	e = slave_request_relay(peer_get_mdhost(mhpeer), r, peer, command,
 	    format, app, isref);
 	if (e != GFARM_ERR_NO_ERROR) {
 		gflog_error(GFARM_MSG_UNFIXED,
@@ -591,7 +559,7 @@ gfm_server_relay_put_vrequest(struct peer *peer,
 		free(r);
 	}
 
-	abstract_host_sender_unlock(ah, mhpeer, diag);
+	gfmdc_ensure_remote_peer_end(mhpeer);
 
 	/*
 	 * *rp will be freed asynchronously in
@@ -1076,7 +1044,6 @@ request_reply_dynarg_slave(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 	char *buf = NULL;
 	size_t rsz, size = 0;
 	gfarm_uint64_t seqnum, flags;
-	struct abstract_host *ah;
 	struct peer *mhpeer;
 	gfp_xdr_async_peer_t async_server = NULL;
 	struct relayed_request *r = NULL;
@@ -1100,21 +1067,20 @@ request_reply_dynarg_slave(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 	 * slave_request_relay_disconnect()
 	 * after relayed_request_acquire_notify() is called.
 	 */
-	if ((e = ensure_remote_peer(peer, command, diag, &r))
-	    != GFARM_ERR_NO_ERROR) {
-		gflog_debug(GFARM_MSG_UNFIXED,
-		    "%s: %s (ensure_remote_peer): %s",
-		    diag, relay_diag, gfarm_error_string(e));
-		goto end;
+	r = relayed_request_new(command, diag);
+	if (r == NULL) {
+		e = GFARM_ERR_NO_MEMORY;
+		gflog_error(GFARM_MSG_UNFIXED,
+		    "%s", gfarm_error_string(e));
+		return (e);
 	}
 
-	ah = mdhost_to_abstract_host(mdhost_lookup_master());
-	if ((e = abstract_host_sender_lock(ah, &mhpeer, diag))
+	if ((e = gfmdc_ensure_remote_peer(peer, &mhpeer))
 	    != GFARM_ERR_NO_ERROR) {
 		gflog_debug(GFARM_MSG_UNFIXED,
-		    "%s: %s (abstract_host_sender_lock): %s",
-		    diag, relay_diag, gfarm_error_string(e));
-		goto end;
+		    "%s: %s (gfmdc_ensure_remote_peer)",
+		    diag, gfarm_error_string(e));
+		return (e);
 	}
 
 	/*
@@ -1161,7 +1127,7 @@ request_reply_dynarg_slave(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 	}
 
 unlock_sender:
-	abstract_host_sender_unlock(ah, mhpeer, diag);
+	gfmdc_ensure_remote_peer_end(mhpeer);
 
 	if (!xid_allocated)
 		goto end;

server/gfmd/abstract_host.c
@@ -73,6 +73,7 @@ abstract_host_init(struct abstract_host *h, struct abstract_host_ops *ops,
 	h->can_send = 1;
 	h->can_receive = 1;
 	h->is_active = 0;
+	h->peer_generation = 0;
 
 	gfarm_cond_init(&h->ready_to_send, diag, "ready_to_send");
 	gfarm_cond_init(&h->ready_to_receive, diag, "ready_to_receive");
@@ -207,11 +208,38 @@ abstract_host_get_peer(struct abstract_host *h, const char *diag)
 	return (peer);
 }
 
+/*
+ * If abstract_host_get_peer_by_generation() is called and non-NULL value
+ * is returned, the same number of abstract_host_put_peer() calls should be
+ * made.
+ */
+struct peer *
+abstract_host_get_peer_by_generation(struct abstract_host *h,
+    gfarm_uint32_t generation, const char *diag)
+{
+	struct peer *peer = NULL;
+
+	abstract_host_mutex_lock(h, diag);
+	if (h->peer_generation == generation && h->peer != NULL) {
+		peer = h->peer;
+		peer_add_ref(peer);
+	}
+	abstract_host_mutex_unlock(h, diag);
+
+	return (peer);
+}
+
 void
 abstract_host_put_peer(struct abstract_host *h, struct peer *peer)
 {
 	if (peer != NULL)
 		peer_del_ref(peer);
+}
+
+gfarm_uint32_t
+abstract_host_get_peer_generation(struct abstract_host *h)
+{
+	return (h->peer_generation);
 }
 
 struct netsendq *
@@ -373,6 +401,7 @@ abstract_host_set_peer(struct abstract_host *h, struct peer *p, int version)
 	h->protocol_version = version;
 	h->is_active = 1;
 	h->ops->set_peer_locked(h, p);
+	h->peer_generation++;
 
 	peer_add_ref(p);
 
@@ -743,7 +772,7 @@ async_client_vsend_wrapped_request_unlocked(struct abstract_host *host,
 	const char *wrapping_format, va_list *wrapping_app,
 	gfarm_int32_t command, const char *format, va_list *app, int isref)
 {
-	gfarm_error_t e;
+	gfarm_error_t e = GFARM_ERR_NO_ERROR;
 	struct peer *peer = host->peer; /* OK, if sender_lock is held */
 	gfp_xdr_async_peer_t async;
 	struct gfp_xdr *server;

server/gfmd/abstract_host.h
@@ -24,7 +24,10 @@ int abstract_host_is_up(struct abstract_host *);
 const char *abstract_host_get_name(struct abstract_host *);
 int abstract_host_get_port(struct abstract_host *);
 struct peer *abstract_host_get_peer(struct abstract_host *, const char *);
+struct peer *abstract_host_get_peer_by_generation(struct abstract_host *,
+	gfarm_uint32_t, const char *);
 void abstract_host_put_peer(struct abstract_host *, struct peer *);
+gfarm_uint32_t abstract_host_get_peer_generation(struct abstract_host *);
 void abstract_host_set_peer(struct abstract_host *, struct peer *, int);
 struct netsendq *abstract_host_get_sendq(struct abstract_host *);
 void abstract_host_disconnect_request(struct abstract_host *, struct peer *,
@@ -51,6 +54,10 @@ gfarm_error_t async_server_vput_reply(struct abstract_host *,
 	struct peer *, gfp_xdr_xid_t,
 	gfarm_error_t (*xdr_vsend)(struct gfp_xdr *, const char **, va_list *),
 	const char *, gfarm_error_t, const char *, va_list *);
+gfarm_error_t async_server_put_reply(struct abstract_host *,
+	struct peer *, gfp_xdr_xid_t,
+	gfarm_error_t (*xdr_vsend)(struct gfp_xdr *, const char **, va_list *),
+	const char *, gfarm_error_t, const char *, ...);
 gfarm_error_t async_server_vput_wrapped_reply_unlocked(struct abstract_host *,
 	gfp_xdr_xid_t,
 	xdr_vsend_t, const char *, gfarm_error_t,
@@ -84,6 +91,9 @@ gfarm_error_t async_client_vsend_request(struct abstract_host *,
 	host_set_callback_t,
 #endif
 	gfarm_int32_t, const char *, va_list *);
+gfarm_error_t async_client_send_raw_request(struct abstract_host *,
+	struct peer *, const char *, result_callback_t,
+	disconnect_callback_t, void *, size_t, void *);
 gfarm_error_t async_client_vrecv_result(struct peer *,
 	struct abstract_host *, size_t, const char *, const char **,
 	gfarm_error_t *, va_list *);

server/gfmd/abstract_host_impl.h
@@ -29,6 +29,7 @@ struct abstract_host {
 	struct peer *peer;
 	int protocol_version;
 	int is_active;
+	gfarm_uint32_t peer_generation;
 
 	struct netsendq *sendq;
 };

server/gfmd/back_channel.c
@@ -523,6 +523,7 @@ gfm_server_switch_back_channel_common(
 			    "%s: gfp_xdr_async_peer_new(): %s",
 			    diag, gfarm_error_string(e));
 		}
+		peer_set_peer_type(peer, peer_type_back_channel);
 		giant_unlock();
 	}
 	if (version < GFS_PROTOCOL_VERSION_V2_4)

server/gfmd/gfmd_channel.c
@@ -74,6 +74,7 @@ static const char REQUEST_WAIT_INFO_COND[] = "request_sync_info.cond";
 static const char PEER_RECORD_MUTEX_DIAG[] = "gfmdc_peer_record.mutex";
 
 #define GFMDC_CONNECT_INTERVAL	30
+#define GFMDC_REMOTE_PEER_ALLOC_MAX_RETRY_COUNT	2
 
 /*
  * gmfdc_journal_send_closure
@@ -965,10 +966,7 @@ gfmdc_client_remote_peer_alloc(struct peer *peer)
 	    GFM_PROTO_REMOTE_PEER_ALLOC, "lissiii",
 	    peer_get_id(peer), peer_get_auth_id_type(peer),
 	    peer_get_username(peer), peer_get_hostname(peer),
-	    proto_family, proto_transport, port)) == GFARM_ERR_NO_ERROR)
-		local_peer_set_remote_peer_allocated(peer_to_local_peer(peer),
-		    1);
-	else
+	    proto_family, proto_transport, port)) != GFARM_ERR_NO_ERROR)
 		gflog_error(GFARM_MSG_UNFIXED,
 		    "%s: %s", diag, gfarm_error_string(e));
 	return (e);
@@ -995,6 +993,73 @@ gfmdc_server_remote_peer_alloc(struct mdhost *mh, struct peer *peer,
 	}
 	e = gfmdc_server_put_reply(mh, peer, xid, diag, e, "");
 	return (e);
+}
+
+/* 
+ * Perform a GFM_PROTO_REMOTE_PEER_ALLOC request for 'peer' if needed.
+ *
+ * This function requests GFM_PROTO_REMOTE_PEER_ALLOC again if the slave
+ * gfmd has reconnected to a master gfmd.
+ *
+ * Upon successful, this function locks a peer to the current master
+ * gfmd and returns its peer as '*mhpeerp'.  The caller of this function
+ * must call gfmdc_ensure_remote_peer_end() to unlock the peer.
+ */
+gfarm_error_t
+gfmdc_ensure_remote_peer(struct peer *peer, struct peer **mhpeerp)
+{
+	gfarm_error_t e;
+	struct local_peer *lpeer = peer_to_local_peer(peer);
+	struct abstract_host *mah, *lpeer_mah;
+	struct peer *my_mhpeer = NULL;
+	int retry_count = 0;
+	gfarm_uint32_t gen, lpeer_gen;
+	const char diag[] = "gfmdc_ensure_remote_peer";
+
+	for (;;) {
+		mah = mdhost_to_abstract_host(mdhost_lookup_master());
+		e = abstract_host_sender_lock(mah, &my_mhpeer, diag);
+		if (e != GFARM_ERR_NO_ERROR) {
+			gflog_error(GFARM_MSG_UNFIXED,
+			    "%s (abstract_host_sender_lock): %s",
+			    diag, gfarm_error_string(e));
+			return (e);
+		}
+		gen = abstract_host_get_peer_generation(mah);
+		if (local_peer_get_remote_peer_allocated(lpeer, &lpeer_mah,
+			&lpeer_gen) && lpeer_mah == mah && lpeer_gen == gen)
+			break;
+		abstract_host_sender_unlock(mah, my_mhpeer, diag);
+
+		if (retry_count >= GFMDC_REMOTE_PEER_ALLOC_MAX_RETRY_COUNT) {
+			gflog_error(GFARM_MSG_UNFIXED, "%s: retry overtime",
+			    diag);
+			return (e);
+		}
+
+		e = gfmdc_client_remote_peer_alloc(peer);
+		if (e != GFARM_ERR_NO_ERROR) {
+			gflog_error(GFARM_MSG_UNFIXED,
+			    "%s (gfmdc_client_remote_peer_alloc): %s",
+			    diag, gfarm_error_string(e));
+			return (e);
+		}
+		local_peer_set_remote_peer_allocated(lpeer, mah, gen);
+		retry_count++;
+	}
+
+	*mhpeerp = my_mhpeer;
+	return (GFARM_ERR_NO_ERROR);
+}
+
+void
+gfmdc_ensure_remote_peer_end(struct peer *mhpeer)
+{
+	struct abstract_host *mah;
+	const char diag[] = "gfmdc_ensure_remote_peer_end";
+
+	mah = peer_get_abstract_host(mhpeer);
+	abstract_host_sender_unlock(mah, mhpeer, diag);
 }
 
 static gfarm_error_t
@@ -1261,6 +1326,7 @@ switch_gfmd_channel(struct peer *peer, int from_client,
 		    diag, gfarm_error_string(e));
 		gfmdc_peer_record_free(gfmdc_peer, diag);
 	}
+	peer_set_peer_type(peer, peer_type_gfmd_channel);
 	giant_unlock();
 	if (e == GFARM_ERR_NO_ERROR) {
 		struct local_peer *local_peer = peer_to_local_peer(peer);

server/gfmd/gfmd_channel.h
@@ -14,3 +14,5 @@ void *gfmdc_journal_asyncsend_thread(void *);
 void *gfmdc_connect_thread(void *);
 gfarm_error_t gfmdc_client_remote_peer_alloc(struct peer *);
 gfarm_error_t gfmdc_client_remote_peer_free(gfarm_uint64_t);
+gfarm_error_t gfmdc_ensure_remote_peer(struct peer *, struct peer **);
+void gfmdc_ensure_remote_peer_end(struct peer *);

server/gfmd/local_peer.c
@@ -47,6 +47,9 @@ struct local_peer {
 	struct gfp_xdr *conn;
 	gfp_xdr_async_peer_t async; /* used by {back|gfmd}_channel */
 
+	struct abstract_host *master_abhost;
+	gfarm_uint32_t master_generation;
+
 	struct peer_watcher *readable_watcher;
 	struct watcher_event *readable_event;
 
@@ -70,6 +73,12 @@ struct peer *
 local_peer_to_peer(struct local_peer *local_peer)
 {
 	return (&local_peer->super);
+}
+
+enum peer_type
+local_peer_get_peer_type(struct local_peer *local_peer)
+{
+	return (peer_get_peer_type(&local_peer->super));
 }
 
 static struct local_peer *
@@ -329,7 +338,7 @@ local_peer_free(struct peer *peer)
 	struct local_peer *local_peer = peer_to_local_peer(peer);
 	gfarm_uint64_t peer_id = peer_get_id(peer);
 	int remote_peer_allocated = local_peer_get_remote_peer_allocated(
-		local_peer);
+	    local_peer, NULL, NULL);
 	int peer_is_master_gfmd = 0;
 	static const char diag[] = "local_peer_free";
 
@@ -445,6 +454,8 @@ local_peer_alloc0(int fd, struct gfp_xdr *conn,
 	}
 	local_peer->conn = conn;
 	local_peer->async = NULL; /* synchronous protocol by default */
+	local_peer->master_abhost = NULL;
+	local_peer->master_generation = 0;
 
 	if (local_peer->readable_event == NULL) {
 		e = watcher_fd_readable_event_alloc(fd,
@@ -550,6 +561,31 @@ local_peer_watch_readable(struct local_peer *local_peer)
 	    local_peer->readable_event, local_peer);
 }
 
+struct local_peer *
+local_peer_lookup(struct local_peer *parent_peer,
+	gfarm_int64_t peer_id)
+{
+	struct local_peer *result = NULL;
+	int i;
+	static const char diag[] = "local_peer_lookup";
+
+	gfarm_mutex_lock(&local_peer_table_mutex, diag,
+	    local_peer_table_diag);
+
+	for (i = 0; i < local_peer_table_size; i++) {
+		if (local_peer_table[i].super.process != NULL &&
+		    local_peer_table[i].super.peer_id == peer_id) {
+			result = &local_peer_table[i];
+			break;
+		}
+			
+	}
+
+	gfarm_mutex_unlock(&local_peer_table_mutex, diag,
+	    local_peer_table_diag);
+	return result;
+}
+
 /*
  * if local_peer_lookup_remote() is called,
  * the same number of peer_del_ref() calls should be made for the
@@ -646,19 +682,31 @@ local_peer_init(int max_peers)
 }
 
 int
-local_peer_get_remote_peer_allocated(struct local_peer *peer)
+local_peer_get_remote_peer_allocated(struct local_peer *peer,
+	struct abstract_host **master_abhost,
+	gfarm_uint32_t *master_generation)
 {
+	if (master_abhost != NULL)
+		*master_abhost = peer->master_abhost;
+	if (master_generation != NULL)
+		*master_generation = peer->master_generation;
 	return (local_peer_to_peer(peer)->flags &
 	    PEER_FLAGS_REMOTE_PEER_ALLOCATED);
 }
 
 void
-local_peer_set_remote_peer_allocated(struct local_peer *peer, int enabled)
+local_peer_set_remote_peer_allocated(struct local_peer *peer,
+	struct abstract_host *master_abhost, gfarm_uint32_t master_generation)
 {
-	if (enabled)
-		local_peer_to_peer(peer)->flags |=
-		    PEER_FLAGS_REMOTE_PEER_ALLOCATED;
-	else
-		local_peer_to_peer(peer)->flags &=
-		    ~PEER_FLAGS_REMOTE_PEER_ALLOCATED;
+	local_peer_to_peer(peer)->flags |= PEER_FLAGS_REMOTE_PEER_ALLOCATED;
+	peer->master_abhost     = master_abhost;
+	peer->master_generation = master_generation;
+}
+
+void
+local_peer_unset_remote_peer_allocated(struct local_peer *peer)
+{
+	local_peer_to_peer(peer)->flags &= ~PEER_FLAGS_REMOTE_PEER_ALLOCATED;
+	peer->master_abhost     = NULL;
+	peer->master_generation = 0;
 }

commit 0129052386da17949f8b05ce6d6fbca6a695aac2

fix memory leak and ungraceful protocol handling in an error case, introduced in r7862 as part of transition from ensure_remote_peer() to gfmdc_ensure_remote_peer() for #287 (inter-gfmd RPC forwarding)
git-svn-id: svn://svn.code.sf.net/p/gfarm/code/gfarm_v2/trunk@7906 4a5e1ece-6a47-475c-b646-731d720b41d0

server/gfmd/relay.c
@@ -538,28 +538,30 @@ gfm_server_relay_put_vrequest(struct peer *peer,
 	r = relayed_request_new(command, diag);
 	if (r == NULL) {
 		e = GFARM_ERR_NO_MEMORY;
-		gflog_error(GFARM_MSG_UNFIXED,
-		    "%s", gfarm_error_string(e));
+		gflog_error(GFARM_MSG_UNFIXED, "%s: relayed_request_new: %s",
+		    diag, gfarm_error_string(e));
 		return (e);
 	}
-	*rp = r;
 
 	if ((e = gfmdc_ensure_remote_peer(peer, &mhpeer))
 	    != GFARM_ERR_NO_ERROR) {
-		gflog_debug(GFARM_MSG_UNFIXED, "%s", gfarm_error_string(e));
+		gflog_debug(GFARM_MSG_UNFIXED,
+		    "%s: gfmdc_ensure_remote_peer: %s",
+		    diag, gfarm_error_string(e));
+		free(r);
 		return (e);
 	}
-
 	e = slave_request_relay(peer_get_mdhost(mhpeer), r, peer, command,
 	    format, app, isref);
+	gfmdc_ensure_remote_peer_end(mhpeer);
+
 	if (e != GFARM_ERR_NO_ERROR) {
 		gflog_error(GFARM_MSG_UNFIXED,
 		    "%s: slave_request_relay(): %s",
 		    diag, gfarm_error_string(e));
 		free(r);
+		return (e);
 	}
-
-	gfmdc_ensure_remote_peer_end(mhpeer);
 
 	/*
 	 * *rp will be freed asynchronously in
@@ -1070,9 +1072,9 @@ request_reply_dynarg_slave(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 	r = relayed_request_new(command, diag);
 	if (r == NULL) {
 		e = GFARM_ERR_NO_MEMORY;
-		gflog_error(GFARM_MSG_UNFIXED,
-		    "%s", gfarm_error_string(e));
-		return (e);
+		gflog_error(GFARM_MSG_UNFIXED, "%s: relayed_request_new: %s",
+		    diag, gfarm_error_string(e));
+		goto end;
 	}
 
 	if ((e = gfmdc_ensure_remote_peer(peer, &mhpeer))
@@ -1080,7 +1082,8 @@ request_reply_dynarg_slave(struct peer *peer, gfp_xdr_xid_t xid, int skip,
 		gflog_debug(GFARM_MSG_UNFIXED,
 		    "%s: %s (gfmdc_ensure_remote_peer)",
 		    diag, gfarm_error_string(e));
-		return (e);
+		free(r);
+		goto end;
 	}
 
 	/*